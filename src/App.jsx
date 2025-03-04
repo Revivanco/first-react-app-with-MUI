1a8c76e117b9967304fee79bb2d06eb7064f0a8a
Regina Vivanco Lemus <revilem1331@gmail.com>
2025-03-03 18:33:09 -0600

    React file was created with UI material

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+    </div>
+  );
+}
+
+export default App;
